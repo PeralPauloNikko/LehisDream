f7ad0e0f1f6a4dbb2feba51291c0ae54d29896aa
EthicalH1 <EthicalH1@Nikko>
2016-10-08 18:39:26 -0400

    10/7/2016 - Initial commit to repository 8:48 pm

---
+++ LehisDream/src/lehisdream/LehisDream2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lehisdream;
+
+import byui.cit260.LehisDream.model.BackPack;
+import byui.cit260.LehisDream.model.Person;
+import byui.cit260.LehisDream.model.Map;
+import byui.cit260.LehisDream.model.Item;
+import byui.cit260.LehisDream.model.Question;
+import byui.cit260.LehisDream.model.AtStore;
+import byui.cit260.LehisDream.model.AtTheatre;
+/**
+ *
+ * @author EthicalH1
+ */
+public class LehisDream2 {
+    
+    public static void nikkoTest(String[] args) {
+    //Backpack test
+    
+    
+    
+    }
+    
+    
+    
+}
+
+
