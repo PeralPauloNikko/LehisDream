fb1b9a8c30aa3fdd20382123d002bb081ac0790b
smith <smith@smith-PC>
2016-10-29 21:43:58 -0700

    Merge origin/master
    Conflicts:
    	LehisDream/src/byui/cit260/LehisDream/control/GameControl.java
    	LehisDream/src/byui/cit260/LehisDream/control/InventoryControl.java
    	LehisDream/src/byui/cit260/LehisDream/control/MapControl.java
    	LehisDream/test/byui/cit260/LehisDream/control/MapControlTest.java

--- LehisDream/src/byui/cit260/LehisDream/control/GameControl.java
+++ LehisDream/src/byui/cit260/LehisDream/control/GameControl.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package byui.cit260.LehisDream.control;
+import byui.cit260.LehisDream.model.Player;
+import lehisdream.LehisDream;
 
 /**
  *
  * @author smith
  */
 public class GameControl {
+
+    public static Player createPlayer(String name) {
+        if (name == null) {
+            return null;
+        }
+        Player player = new Player();
+        player.setName(name);
+        
+        LehisDream.setPlayer(player);//save the player
+        
+        return player;
+        
+    }
+    }
     
-}
+
