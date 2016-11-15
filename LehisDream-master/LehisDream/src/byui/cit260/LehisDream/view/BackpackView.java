3f625fdc2dbd2bc1362790d37b0f71591134c279
smith <smith@smith-PC>
2016-11-14 22:12:10 -0700

    Merge origin/master
    Conflicts:
    	LehisDream/src/byui/cit260/LehisDream/control/GameControl.java
    	LehisDream/src/byui/cit260/LehisDream/control/InventoryControl.java
    	LehisDream/src/byui/cit260/LehisDream/control/MapControl.java
    	LehisDream/test/byui/cit260/LehisDream/control/MapControlTest.java

--- LehisDream-master/LehisDream/src/byui/cit260/LehisDream/view/BackpackView.java
+++ LehisDream-master/LehisDream/src/byui/cit260/LehisDream/view/BackpackView.java
@@ -16,58 +16,16 @@ public class BackpackView extends View {
 public BackpackView() {
           super("\n"
               + "\n-------------------------------------------"
-              + "\n| Main Menu                               |"
+              + "\n|              Backpack                   |"
               + "\n-------------------------------------------"
-              + "\nS - Sword(can only be used once)-----------"
-              + "\nF - Flashlight-----------------------------"
-              + "\nD - Water(1)-------------------------------"
-              + "\nE - Snack(1)-------------------------------"
-              + "\nW - Wallet---------------------------------"
-              + "\nQ - Quit-----------------------------------"
+              + "\nS - Sword(can only be used once)"
+              + "\nF - Flashlight"
+              + "\nD - Water(1)"
+              + "\nE - Snack(1)"
+              + "\nW - Wallet"
+              + "\nQ - Quit"
               + "\n-------------------------------------------");
 }
-    //display item view
-public void displayInput() {
-    boolean done = false;//set flag to not done
-    do {
-        //prompt for and get menu option
-            //prompt for and get menu option
-            String getInput = this.getInput();
-            if (getInput.toUpperCase().equals("Q")) //user wants to quit
-                return; // exit the game
-            
-            //do the requested action and display the next view
-            done = this.doAction(getInput);
-            
-        } while (!done);
-        
-    }
-
-    /**
-     *
-     * @return
-     */
-    public String getInput() {
- 
-        Scanner keyboard = new Scanner(System.in);//get infile for keyboard
-        String value = "";// value to be returned
-        boolean valid = false;// initialize to not valid
-        
-        while (!valid) {// loop while an invalid value is entered
-           System.out.println("\n" + this.displayMessage);
-            
-            value = keyboard.nextLine();// get next line typed on keyboard
-            value = value.trim(); // trim off leading and trailing blanks
-            
-            if (value.length() < 1) {// value is blank
-                System.out.println("\nInvalid value: you cannot leave this blank");
-                continue;
-            }
-            
-            break; // end the loop
-        }
-        return value; //return the value entered
-    }
     @Override
     public boolean doAction(String choice) {
        choice = choice.toUpperCase(); // convert choice to upper case
--- LehisDream-master/LehisDream/src/byui/cit260/LehisDream/view/GameMenuView.java
+++ LehisDream-master/LehisDream/src/byui/cit260/LehisDream/view/GameMenuView.java
@@ -90,7 +90,7 @@ public class GameMenuView extends View{
     private void goToBackpack() {
         System.out.println("*** goToBackpack function called ***");
         BackpackView backPack = new BackpackView();
-        backPack.displayInput();
+        backPack.display();
     }
     private void moveForward() {
         System.out.println("*** moveForward function called ***");          
