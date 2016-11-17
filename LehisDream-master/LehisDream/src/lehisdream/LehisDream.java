9002665672a85ad9f39a99c96ef0d0ff8ee846db
smith <smith@smith-PC>
2016-11-17 12:40:41 -0700

    Merge origin/master
    Conflicts:
    	LehisDream/src/byui/cit260/LehisDream/control/GameControl.java
    	LehisDream/src/byui/cit260/LehisDream/control/InventoryControl.java
    	LehisDream/src/byui/cit260/LehisDream/control/MapControl.java
    	LehisDream/test/byui/cit260/LehisDream/control/MapControlTest.java

--- LehisDream-master/LehisDream/src/lehisdream/LehisDream.java
+++ LehisDream-master/LehisDream/src/lehisdream/LehisDream.java
@@ -5,9 +5,7 @@
  */
 package lehisdream;
 
-import byui.cit260.LehisDream.model.AtChurch;
-import byui.cit260.LehisDream.model.AtHome;
-import byui.cit260.LehisDream.model.AtSchool;
+
 import byui.cit260.LehisDream.model.Player;
 import byui.cit260.LehisDream.model.IronRodGame;
 import byui.cit260.LehisDream.model.Location;
@@ -16,12 +14,11 @@ import byui.cit260.LehisDream.model.TreeOfLife;
 
 //nikkoTest
 import byui.cit260.LehisDream.model.BackPack;
-import byui.cit260.LehisDream.model.Person;
+import byui.cit260.LehisDream.model.BeingEncounter;
 import byui.cit260.LehisDream.model.Map;
 import byui.cit260.LehisDream.model.Item;
 import byui.cit260.LehisDream.model.Question;
-import byui.cit260.LehisDream.model.AtStore;
-import byui.cit260.LehisDream.model.AtTheatre;
+
 import byui.cit260.LehisDream.view.StartProgramView;
 
 /**
@@ -99,32 +96,6 @@ public class LehisDream {
         String sceneInfo = portrait.toString();
         System.out.println(sceneInfo);
         
-        //At Home Test
-        AtHome room = new AtHome();
-        
-        room.setHomeDescription("Living Room");
-        room.setHomeLocation("Zone 1");
-        
-        String homeInfo = room.toString();
-        System.out.println(homeInfo);
-       
-        //At School Test
-        AtSchool classroom = new AtSchool();
-        
-        classroom.setSchoolDescription("Classroom");
-        classroom.setSchoolLocation("Zone 2");
-        
-        String schoolInfo = classroom.toString();
-        System.out.println(schoolInfo);
-        
-        //At Church Test
-        AtChurch facility = new AtChurch();
-        
-        facility.setChurchDescription("Chapel");
-        facility.setChurchLocation("Zone 5");
-        
-        String churchInfo = facility.toString();
-        System.out.println(churchInfo);
     }
     
     
@@ -138,7 +109,7 @@ public class LehisDream {
     System.out.println(backPackOne);
     
     //Person Test
-    Person personOne = new Person();
+    BeingEncounter personOne = new BeingEncounter();
     
     personOne.setCoordinates("Home");
     personOne.setPositiveNegativeEnergy(100);
@@ -179,25 +150,7 @@ public class LehisDream {
     String questionOneInfo = questionOne.toString();
     System.out.println(questionOneInfo);
     
-    //AtStore Test
-    AtStore buySell = new AtStore();
-    
-    buySell.setStoreDescription("Buy and Sell Stuff for your long journey!");
-    buySell.setGetPurchasedItem("Energy Bar, Water, Energy Drink");
-    
-    String buySellInfo = buySell.toString();
-    System.out.println(buySellInfo);
-    
-    //AtTheatre Test
-    AtTheatre theatreLocation = new AtTheatre();
-    
-    theatreLocation.setAtTheatreDescription("The theatre! It's dark inside!");
-    theatreLocation.setCalculateAdmission("$25");
-    
-    String theatreLocationInfo = theatreLocation.toString();
-    System.out.println(theatreLocationInfo);
-    
-    
+   
     
     
     }
